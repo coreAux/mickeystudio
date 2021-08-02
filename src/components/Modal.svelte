86d50a15530eebc7cf5c4b2ac210db431b2aee5f
Mikael Petersen <info@mikaelpetersen.se>
2021-08-02 19:25:49 +0100

    fixing modal on small screen

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -4,7 +4,7 @@
     top: 0;
     left: 0;
     width: 100vw;
-    height: 100vh;
+    height: calc(100vh + 240px);
     background-color: rgba(0, 0, 0, .2);
     background: linear-gradient(90deg, rgba(255, 0, 0, .8), rgba(255, 0, 255, .8)), linear-gradient(180deg, rgba(0, 0, 0, .8), rgba(255, 255, 0, .8));
     background-blend-mode: exclusion;
